5b4e6ba43e551048da836e5aae9111cda537b6d4
jpd61 <jpd@dewoodyoil.com>
2020-06-23 22:18:43 -0500

    working with implementing lodash into helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 
 // Test helper as placeholder
 module.exports = {
@@ -12,5 +13,13 @@ module.exports = {
         }
     
         return word;
+    },
+    // Removes Diacritical marks from names
+    format_deburr: _.deburr(word),
+    // Run API through to test if it is empty (finish function)
+    format_isEmpty: (obj) => {
+        if _.isEmpty(obj) {
+            return err
+        }
     }
   }
