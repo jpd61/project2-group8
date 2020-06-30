5c4027157fe4597fece1f53b954ccd107c27e1a3
Chris Short <twitchshort@gmail.com>
2020-06-30 05:05:56 -0500

    add whiskey handlebars and attempt to connect to routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Whiskey, User, Comment } = require('../models');
+
 
 router.get('/', (req, res) => {
     console.log(req.session);
